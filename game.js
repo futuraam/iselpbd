5e38f4a1fdefd87d79f7bf7e858d9cc4d8735c01
futuraam <51935611+futuraam@users.noreply.github.com>
2019-06-18 13:38:45 -0400

    Update game.js

--- game.js
+++ game.js
@@ -54,7 +54,7 @@ function create() {
     gameState.enemies = this.physics.add.group();
   for (let yVal = 1; yVal < 4; yVal++) {
     for (let xVal = 1; xVal < 9; xVal++) {
-      gameState.enemies.create(80 * xVal, 80 * yVal, 'bug1')
+      gameState.enemies.create(80 * xVal, 90 * yVal, 'bug1')
       .setScale(.96)
       .setGravityY(-200);
     };
